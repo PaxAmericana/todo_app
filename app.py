8b1a2190a0aeece70dd2f6b0bbdb66a21f3a4c91
Pax_Americana <paxton.e.wills@gmail.com>
2020-04-23 15:18:34 -0400

    added database connection with sqlite3

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 from flask import Flask, render_template, request, redirect
-
 app = Flask(__name__)
 
-todos = [
-        "go to the allergy office",
-        "watch the databases video while you're there",
-        "try to work on SQL via the web course so that you can complete CETO.. all friday/saturday",
-]
+import sqlite3
+conn = sqlite3.connect('registrants.db')
+db = conn.cursor()
+conn.row_factory = sqlite3.Row
+
 
 @app.route('/')
 def tasks():
+    with sqlite3.connect('registrants.db') as conn:
+        conn.row_factory = sqlite3.Row
+        db = conn.cursor()
+
+        db.execute("SELECT * FROM registrants")
+        todos = db.fetchall()
     return render_template('tasks.html', todos=todos)
 # GET requests allowed by default
 # allow POST requests, which will constain a FORM which contains info that you can choose to POST
-@app.route('/add_task', methods =["GET", "POST"])
+@app.route('/add_task', methods =["POST"])
 def add_task():
-    if request.method == 'GET':
-        return render_template('add_task.html')
+    # if request.method == 'GET':
+    #     return render_template('add_task.html')
     if request.method == "POST":
-        todo = request.form.get('task')
-        todos.append(todo)
+        with sqlite3.connect('registrants.db') as conn:
+            conn.row_factory = sqlite3.Row
+            db = conn.cursor()
+            name = request.form.get('name')
+            email = request.form.get('email')
+            db.execute("INSERT INTO registrants (name, email) VALUES (?, ?)", [name, email])
+            conn.commit()
         return redirect('/')
         # rather than past render_template(tasks.html many times, the tasks function will handle that for me, just tell it to go to that function
         #return render_template(tasks.html, todos=todos)
