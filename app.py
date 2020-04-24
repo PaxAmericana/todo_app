04766c32cb88216ede3d09901cba5d0c7a038c9a
Pax_Americana <paxton.e.wills@gmail.com>
2020-04-24 09:08:08 -0400

    sessions not working completely, basic todo section

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
 from flask import Flask, render_template, request, redirect
+from tempfile import mkdtemp
+
 app = Flask(__name__)
 
 import sqlite3
 conn = sqlite3.connect('registrants.db')
-db = conn.cursor()
 conn.row_factory = sqlite3.Row
+db = conn.cursor()
+from flask import session
+from flask_session import Session
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+from cachelib.file import FileSystemCache
+# session gives you access to a python dictionary
+Session(app)
+
+with sqlite3.connect('registrants.db') as conn:
+    conn.row_factory = sqlite3.Row
+    db = conn.cursor()
+    db.execute("SELECT * FROM registrants")
+    rows = db.fetchall()
+    todos = rows
+    # i'm really not sure how to use these sessions properly
+    with app.test_request_context():
+        session['todos'] = rows
 
 @app.route('/')
 def tasks():
-    with sqlite3.connect('registrants.db') as conn:
-        conn.row_factory = sqlite3.Row
-        db = conn.cursor()
+    # with sqlite3.connect('registrants.db') as conn:
+    #     conn.row_factory = sqlite3.Row
+    #     db = conn.cursor()
+    #
+    #     db.execute("SELECT * FROM registrants")
+    #     todos = db.fetchall()
+    data= session['todos']
+    print(data)
+    return render_template('tasks.html', todos=data)
 
-        db.execute("SELECT * FROM registrants")
-        todos = db.fetchall()
-    return render_template('tasks.html', todos=todos)
 # GET requests allowed by default
 # allow POST requests, which will constain a FORM which contains info that you can choose to POST
 @app.route('/add_task', methods =["POST"])
